1b2c02938e0f376741f5ab6c85a4a1d8d69a66ea
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2023-12-31 00:17:55 +0000

    Acrescento da restrição do lucro do pacote e variável Z. Alteração na restrição de capacidade total, ainda só funciona para o input2

--- p3.py
+++ p3.py
@@ -7,6 +7,8 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
     # Variáveis de decisão
     x = LpVariable.dicts("brinquedo", range(1, t + 1), 0, None, LpInteger)
     y = LpVariable.dicts("pacote", range(1, p + 1), 0, None, LpInteger)
+    # z indica se o brinquedo i está no pacote j, 1 se sim, 0 se não
+    z = LpVariable.dicts("no_pacote", ((i,j) for i in range(1, t + 1) for j in range(1, p + 1)), cat=LpBinary)
 
     # Função objetivo
     modelo += lpSum(brinquedos[i-1][0] * x[i] for i in range(1, t + 1)) + \
@@ -15,11 +17,15 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
     # Restrições de capacidade de produção de cada brinquedo
     for i in range(1, t + 1):
         modelo += x[i] <= brinquedos[i-1][1], f"Capacidade_Producao_Brinquedo_{i}"
-
+        
     # Restrição de capacidade total de produção
     modelo += lpSum(x[i] for i in range(1, t + 1)) \
-            + lpSum(pacotes[j-1][3] * y[j] for j in range(1, p + 1)) <= max_brinquedos, "Capacidade_Total"
-
+            + lpSum(3 * y[j] for j in range(1, p + 1)) <= max_brinquedos, "Capacidade_Total"
+            
+    # Restrição do lucro do pacote
+    for j in range(1, p + 1):
+        modelo += lpSum(brinquedos[i-1][0] * z[i,j] for i in range(1, t + 1)) <= pacotes[j-1][3] * y[j], f"Lucro_Pacote_{j}"
+    
     modelo.solve(GLPK(msg=0))
 
     return int(value(modelo.objective))
