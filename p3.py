761248f183de87a0d6cc6b5b9ea4f1ce394dfd12
Inês Paredes <ines.paredes@tecnico.ulisboa.pt>
2023-12-31 11:53:17 +0000

    Remoção de restrições desnecessárias, mas continua a funcionar para o input2

--- p3.py
+++ p3.py
@@ -5,34 +5,27 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
     modelo = LpProblem("Maximizar_Lucro", LpMaximize)
 
     # Variáveis de decisão
-    x = LpVariable.dicts("brinquedo", range(t), 0, max_brinquedos, LpInteger)
-    y = LpVariable.dicts("pacote", range(p), 0, None, LpInteger)
+    x = LpVariable.dicts("brinquedo", range(1, t + 1), 0, None, LpInteger)
+    y = LpVariable.dicts("pacote", range(1, p + 1), 0, None, LpInteger)
 
     # Função objetivo
-    modelo += lpSum(brinquedos[i][0] * x[i] for i in range(t)) + \
-                lpSum(pacotes[j][3] * y[j] for j in range(p)), "Lucro_Total"
+    modelo += lpSum(brinquedos[i-1][0] * x[i] for i in range(1, t + 1)) + \
+              lpSum(pacotes[j-1][3] * y[j] for j in range(1, p + 1)), "Lucro_Total"
 
-    # Restrições de capacidade de produção
-    for i in range(t):
-        modelo += x[i] <= brinquedos[i][1], f"Capacidade_Producao_Brinquedo_{i}"
+    # Restrições de capacidade de produção de cada brinquedo
+    for i in range(1, t + 1):
+        modelo += x[i] <= brinquedos[i-1][1], f"Capacidade_Producao_Brinquedo_{i}"
 
-    # Restrições de capacidade total
-    modelo += lpSum(x[i] for i in range(t)) <= max_brinquedos, "Capacidade_Total"
-              
-    # Restrições de pacotes
-    for j in range(p):
-        # Verifica se a chave existe no dicionário antes de acessar
-        if pacotes[j][:3] and all(i < t for i in pacotes[j][:3]):
-            modelo += lpSum(brinquedos[i][0] for i in pacotes[j][:3]) >= pacotes[j][3], f"Brinquedos_Pacote_{j}"
+    # Restrição de capacidade total de produção
+    modelo += lpSum(x[i] for i in range(1, t + 1)) \
+            + lpSum(pacotes[j-1][3] * y[j] for j in range(1, p + 1)) <= max_brinquedos, "Capacidade_Total"
 
-    # Resolve o modelo
-    modelo.solve()
+    modelo.solve(GLPK(msg=0))
 
-    # Retorna o valor da função objetivo (lucro máximo)
-    return value(modelo.objective)
+    return int(value(modelo.objective))
 
 
-###############################################################################
+################################################################################
 # Leitura dos dados de entrada
 entrada = input().split()
 t, p, max_brinquedos = map(int, entrada[:3])
@@ -42,4 +35,4 @@ pacotes = [tuple(map(int, input().split())) for _ in range(p)]
 # Chamada da função e impressão do resultado
 resultado = maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes)
 print(resultado)
-###############################################################################
+################################################################################
