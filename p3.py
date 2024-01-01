373d06476f270ef48bd616b2771aa4af552018b8
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2024-01-01 16:41:29 +0000

    Acrescento de duas restrições. Prints para visualizar o programa comentados. Input2 funciona, input1 já tem valor mais próximo mas ainda não está correto (devolve 6470)

--- p3.py
+++ p3.py
@@ -9,7 +9,7 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
     y = LpVariable.dicts("pacote", range(1, p + 1), 0, None, LpInteger)
     # z indica se o brinquedo i está no pacote j, 1 se sim, 0 se não
     z = LpVariable.dicts("no_pacote", ((i,j) for i in range(1, t + 1) for j in range(1, p + 1)), cat=LpBinary)
-
+    
     # Função objetivo
     modelo += lpSum(brinquedos[i-1][0] * x[i] for i in range(1, t + 1)) + \
               lpSum(pacotes[j-1][3] * y[j] for j in range(1, p + 1)), "Lucro_Total"
@@ -19,14 +19,29 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
         modelo += x[i] <= brinquedos[i-1][1], f"Capacidade_Producao_Brinquedo_{i}"
         
     # Restrição de capacidade total de produção
-    modelo += lpSum(x[i] for i in range(1, t + 1)) \
-            + lpSum(3 * y[j] for j in range(1, p + 1)) <= max_brinquedos, "Capacidade_Total"
-            
-    # Restrição do lucro do pacote
+    modelo += ((lpSum(x[i] for i in x)) + (3 * lpSum(y[j] for j in y))) <= max_brinquedos, "Capacidade_Total"
+        
+    # Para construir um pacote, é necessário produzir pelo menos 1 de cada brinquedo que o compõe
     for j in range(1, p + 1):
-        modelo += lpSum(brinquedos[i-1][0] * z[i,j] for i in range(1, t + 1)) <= pacotes[j-1][3] * y[j], f"Lucro_Pacote_{j}"
-    
+        for i in range(1, t + 1):
+            if z[i,j] == 1:       # Se o brinquedo i está no pacote j
+                modelo += x[i] >= y[j], f"Brinquedo_{i}_no_Pacote_{j}"
+            
+        # Restrição do lucro do pacote
+    for j in range(1, p + 1):
+        modelo += lpSum(z[i,j] * brinquedos[i-1][0] for i in x) <= pacotes[j-1][3], f"Lucro_Pacote_{j}"
+        
     modelo.solve(GLPK(msg=0))
+
+    # for i in x:
+    #     for j in y:
+    #         print(f"Brinquedo {i} no pacote {j}: {z[i,j].varValue}")
+            
+    # for i in x:
+    #     print(f"Brinquedo {i}: {x[i].varValue}")  # Imprime a quantidade de brinquedos i produzidos
+        
+    # for j in y:
+    #     print(f"Pacote {j}: {y[j].varValue}")   # Imprime a quantidade de pacotes j produzidos
 
     return int(value(modelo.objective))
 
