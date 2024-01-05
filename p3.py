624dd3b5356b8f74a4b63414515cc57461642758
Inês Paredes <ines.paredes@tecnico.ulisboa.pt>
2024-01-05 11:02:37 +0000

    Pequena alteração no z, pk nós não estamos a inicializar bem

--- p3.py
+++ p3.py
@@ -10,6 +10,13 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
     # z indica se o brinquedo i está no pacote j, 1 se sim, 0 se não
     z = LpVariable.dicts("no_pacote", ((i,j) for i in range(1, t + 1) for j in range(1, p + 1)), cat=LpBinary)
     
+    # Preenchendo a variável z com base nos dados
+    for i in range(1, t + 1):
+        for j in range(1, p + 1):
+            z[i, j].value = 0  # Inicialmente, nenhum brinquedo está no pacote
+            if i in pacotes[j - 1][:-1]:
+                z[i, j].value = 1  # Se o brinquedo i está no pacote j, atribui 1
+
     # Função objetivo
     modelo += lpSum(brinquedos[i-1][0] * x[i] for i in range(1, t + 1)) + \
               lpSum(pacotes[j-1][3] * y[j] for j in range(1, p + 1)), "Lucro_Total"
@@ -19,8 +26,9 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
         modelo += x[i] <= brinquedos[i-1][1], f"Capacidade_Producao_Brinquedo_{i}"
         
     # Restrição de capacidade total de produção
-    modelo += ((lpSum(x[i] for i in x)) + (3 * lpSum(y[j] for j in y))) <= max_brinquedos, "Capacidade_Total"
-        
+    modelo += lpSum(x[i] for i in x) + \
+            + lpSum(3 * y[j] for j in range(1, p + 1)) <= max_brinquedos, "Capacidade_Total"
+
     # Para construir um pacote, é necessário produzir pelo menos 1 de cada brinquedo que o compõe
     for j in range(1, p + 1):
         for i in range(1, t + 1):
@@ -33,15 +41,15 @@ def maximizar_lucro(t, p, max_brinquedos, brinquedos, pacotes):
         
     modelo.solve(GLPK(msg=0))
 
-    # for i in x:
-    #     for j in y:
-    #         print(f"Brinquedo {i} no pacote {j}: {z[i,j].varValue}")
+    for i in x:
+        for j in y:
+            print(f"Brinquedo {i} no pacote {j}: {z[i,j].varValue}")
             
-    # for i in x:
-    #     print(f"Brinquedo {i}: {x[i].varValue}")  # Imprime a quantidade de brinquedos i produzidos
+    for i in x:
+        print(f"Brinquedo {i}: {x[i].varValue}")  # Imprime a quantidade de brinquedos i produzidos
         
-    # for j in y:
-    #     print(f"Pacote {j}: {y[j].varValue}")   # Imprime a quantidade de pacotes j produzidos
+    for j in y:
+        print(f"Pacote {j}: {y[j].varValue}")   # Imprime a quantidade de pacotes j produzidos
 
     return int(value(modelo.objective))
 
